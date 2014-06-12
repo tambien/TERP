55d46b63db0fd40b12857657a0c48578213efa98
Yotam Mann <yotammann@gmail.com>
2014-06-12 12:18:33 -0400

    new API + AMD Support

--- TERP.js
+++ TERP.js
@@ -1,132 +1,94 @@
-var TERP = function(){
-
-	function isArray(arr){
-		return Object.prototype.toString.call(arr) === '[object Array]'
+(function (root, factory) {
+	//can run with or without requirejs
+	if (typeof define === "function" && define.amd) {
+		// AMD. Register as an anonymous module.
+		define(function () {
+			var TERP = factory();
+			return TERP;
+		});
+	} else if (typeof root.TERP !== "function") {
+		//make Tone public
+		root.TERP = factory(root);
 	}
+} (this, function () {
 
-	/*=========================================================================
-		SCALING
-	=========================================================================*/
-
-
-	/*
-		@param {number} input 0 - 1
-		@param {number} outputMin
-		@param {number} outputMax
-		@returns {number}
-	*/
-	function interpolate(input, outputMin, outputMax){
-		if (isArray(outputMin) && isArray(outputMax)){
-			if (outputMin.length !== outputMax.length){
-				throw new Error("input and output array must be the same length");
-			}
-			var ret = new Array(outputMin.length);
-			for (var i = 0; i < outputMin.length; i++){
-				var min = outputMin[i];
-				var max = outputMax[i];
-				ret[i] = input*(max - min) + min;
-			}
-			return ret;
-		} else {
-			return input*(outputMax - outputMin) + outputMin;
-		}
-	}
-
-	/*
-		@param {number} input
-		@param {number} inputMin
-		@param {number} inputMax
-		@returns {number} normalizes the input between 0-1
-	*/
-	function normalize(input, inputMin, inputMax){
-		//make sure that min < max
-		if (inputMin > inputMax){
-			var tmp = inputMax;
-			inputMax = inputMin;
-			inputMin = tmp;
-		} else if (inputMin == inputMax){
-			return 0;
-		}
-		return (input - inputMin) / (inputMax - inputMin);
-	}
-
-	
-	/*=========================================================================
-		SCALARS
-	=========================================================================*/
-
-	/*
-		@param {number} input 0-1
-		@returns {number} 
-	*/
-	function linear(x){
-		return x;
-	}
-
-	/*
-		@param {number} input 0-1
-		@returns {number} 
-	*/
-	function exponential(x){
-		return x*x;
-	}
-
-	/*
-		@param {number} input 0-1
-		@returns {number} 
-	*/
-	function logarithmic(x){
-		return Math.log(interpolate(x, 1, 10)) / Math.LN10;
-	}
-
-	/*
-		@param {number} input
-		@param {number} min
-		@param {number} max
-
-		clips the output of input to be between min and max
-	*/
-	function clip(input, min, max){
-		input = Math.max(input, min);
-		input = Math.min(input, max);
-		return input;
-	}
-
-	/*=========================================================================
-		SCALE
-	=========================================================================*/
-
-	function scaleInput(args, scalar){
-		//if it includes the input min/max
-		if (args.length  > 4){
-			var norm = normalize(args[0], args[1], args[2]);
-			var scaled = scalar(norm);
-			var ret = interpolate(scaled, args[3], args[4]);
-			if (args[5]){
-				ret = clip(ret, args[3], args[4]);
-			}
-			return ret;
-		//else no input min/max
-		} else {
-			var scaled = scalar(args[0]);
-			var ret = interpolate(scaled, args[1], args[2]);
-			if (args[3]){
-				ret = clip(ret, args[1], args[2]);
-			}
-			return ret;
-		} 
-	}
-
-
-	return {
-		lin : function(){
-			return scaleInput(arguments, linear);
+	var TERP = {
+		/**
+		 *  normalizes (0-1) a number between inputMin and inputMax
+		 *  @param  {number} inputMin 
+		 *  @param  {number} inputMax 
+		 *  @return {number} 
+		 */
+		normalize : function(input, inputMin, inputMax){
+			return (input - inputMin) / (inputMax - inputMin);
 		},
-		exp : function(){
-			return scaleInput(arguments, exponential);
+		/**
+		 *  interpolates a normalized number to between the outputMin and outputMax
+		 *  @param  {number} input   
+		 *  @param  {number} outputMin
+		 *  @param  {number} outputMax
+		 *  @return {number}           
+		 */
+		interpolate : function(input, outputMin, outputMax){
+			return input * (outputMax - outputMin) + outputMin;
 		},
-		log : function(){
-			return scaleInput(arguments, logarithmic);
+		/**
+		 *  if 3 or 4 inputs:
+		 *  	scales the value from 0-1 range to the output range
+	 	 *  	with an optional exponent
+	 	 *  	
+		 *  if 5 or 6 inputs:
+		 *  	scales the value from the input range to the output range
+		 *  	with an optional exponent
+		 *
+		 *  @return {number} 
+		 */
+		scale : function(){
+			var argLen = arguments.length;
+			var input = arguments[0];
+			var normalized, exped;
+			if (argLen === 3) {
+				return TERP.interpolate(input, arguments[1], arguments[2]);
+			} else if (argLen === 4){
+				exped = Math.pow(input, arguments[3]);
+				return TERP.interpolate(exped, arguments[1], arguments[2]);
+			} else if (argLen === 5){
+				normalized = TERP.normalize(input, arguments[1], arguments[2]);
+				return TERP.interpolate(normalized, arguments[3], arguments[4]);
+			} else if (argLen === 6){
+				normalized = TERP.normalize(input, arguments[1], arguments[2]);
+				exped = Math.pow(normalized, arguments[5]);
+				return TERP.interpolate(exped, arguments[3], arguments[4]);
+			} else {
+				console.error("scale accepts 3 - 6 arguments");
+			}
 		},
-	}
-}();
+		/**
+		 *  like scale, but clips the output to the output range
+		 *  @return {number} 
+		 */
+		map : function(){
+			var interped = TERP.scale.apply(TERP, arguments);
+			//clip the output
+			var argLen = arguments.length;
+			var clipMin, clipMax;
+			if (argLen === 3 || argLen === 4){
+				clipMin = Math.min(arguments[1], arguments[2]);
+				clipMax = Math.max(arguments[1], arguments[2]);
+			} else if (argLen === 5 || argLen === 6){
+				clipMin = Math.min(arguments[3], arguments[4]);
+				clipMax = Math.max(arguments[3], arguments[4]);
+			}
+			//clip the output
+			if (interped > clipMax){
+				return clipMax;
+			} else if (interped < clipMin){
+				return clipMin;
+			} else {
+				return interped;
+			}
+		}	
+	};
+
+	return TERP;
+}));
